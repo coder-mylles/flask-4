8b4509f8c51dfc0b3af48f7e8a0002f5ba6830dd
martin-moringa <martin.mylles@student.moringachool.com>
2021-06-23 02:11:07 +0300

    Add the login route logic

--- app/Templates/vews.py
+++ app/Templates/vews.py
@@ -1,51 +1,42 @@
-
-from . import auth
-from .. import db
-from flask import render_template
-from .. import db, Bcrypt
 from flask import Blueprint, render_template, redirect, url_for, request, flash, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from ..models import User
 from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 from flask_login import login_user,current_user,login_required,logout_user
 
 
-@auth.route("/signup")
-
-
-
-
-
-
-@auth.route('/signup', methods=['GET', 'POST'])
+('/signup', methods=['GET', 'POST'])
 def signup():
-    return  render_template("auth/signup.html")
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = SignupForm()
     if form.validate_on_submit():
         # hashed_password = Bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Account has been  created,you can log in.', 'success')
-        return redirect(url_for('auth.login'))
-    return render_template("auth/signup.html",title="Register", form = form) 
-
-    return  render_template("auth/signup.html") 
-
-
-
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}')"
-
-
-
+ def __repr__(self):
 
 
 
 @auth.route("/login")
-@@ -17,3 +48,8 @@ def login():
-def logout():
-    return render_template("auth/logout.html")
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+    return  render_template("auth/login.html") 
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email =form.email.data).first()
+        if user and(user.password, form.password.data):
+        # if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+        else:
+            flash('Login unsucessful.Please check username and password', 'danger')
+    return render_template("auth/login.html",title="Login", form = form)  
 
+
+
+   def logout():
+    return render_template("auth/logout.html")
